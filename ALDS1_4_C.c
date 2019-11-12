cfc3619dcba425cec92766bf2bbbe40deb680d21
Taishi Kasuga <proxy0721@gmail.com>
2019-11-13 01:04:01 +0900

    Add ALDS1_4_C

--- ALDS1_4_C.c
+++ ALDS1_4_C.c
@@ -1,48 +1,84 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <string.h>
 
-static int
-hash(char *str) {
-  int hv = 1;
+#define ARR_SIZE 1000003
+#define STR_SIZE 16
+#define CMD_SIZE 8
+#define H1(k) (k % ARR_SIZE)
+#define H2(k) (1 + k % (ARR_SIZE - 1))
+#define HASH(k, i) ((H1(k) + i * H2(k)) % ARR_SIZE)
 
-  while (*str != '\0') {
+static long long int to_key(char *str) {
+  long long int p, sum;
+
+  for (p = 1, sum = 0; *str != '\0'; p *= 5) {
     switch (*str++) {
-      case 'T':
-        break;
-      case 'G':
-        break;
-      case 'A':
-        break;
-      case 'C':
-        break;
+      case 'T': sum += 1L * p; break;
+      case 'G': sum += 2L * p; break;
+      case 'A': sum += 3L * p; break;
+      case 'C': sum += 4L * p; break;
     }
   }
+  return sum;
+}
 
-  return hv;
+static void insert(char **arr, char *str) {
+  long long int k;
+  int idx, i;
+
+  k = to_key(str);
+  for (i = 0; i < ARR_SIZE; ++i) {
+    idx = HASH(k, i);
+    if (arr[idx] == NULL) {
+      arr[idx] = str;
+      break;
+    }
+    if (strcmp(arr[idx], str) == 0) break;
+  }
+}
+
+static int find(char **arr, char *str) {
+  long long int k;
+  int idx, i;
+
+  k = to_key(str);
+  for (i = 0; i < ARR_SIZE; ++i) {
+    idx = HASH(k, i);
+    if (arr[idx] == NULL) return 0;
+    if (strcmp(arr[idx], str) == 0) return 1;
+  }
+  return 0;
 }
 
 int main(int argc, char **argv) {
-  int n, ret;
-  char cmd[8];
-  char str[16];
-  int arr[2000000];
+  int n, i, ret;
+  char cmd[CMD_SIZE];
+  char *str;
+  char **arr;
 
   ret = scanf("%d", &n);
   if (errno != 0) perror("scanf: n");
   if (ret < 1) exit(1);
 
+  arr = (char **) calloc(ARR_SIZE, sizeof(char *));
+  if (arr == NULL) exit(1);
+
   while (n--) {
+    str = (char *) calloc(STR_SIZE, sizeof(char));
+    if (str == NULL) break;
+
     ret = scanf("%s %s", cmd, str);
     if (errno != 0) perror("scanf: cmd, str");
     if (ret < 1) break;
 
     switch (cmd[0]) {
       case 'i':
-        arr[hash(str)] = 1;
+        insert(arr, str);
         break;
       case 'f':
-        if (arr[hash(str)]) {
+        if (find(arr, str)) {
           printf("yes\n");
         } else {
           printf("no\n");
@@ -51,5 +87,7 @@ int main(int argc, char **argv) {
     }
   }
 
+  for (i = 0; i < ARR_SIZE; ++i) free(arr[i]);
+  free(arr);
   exit(0);
 }
